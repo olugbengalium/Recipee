617a25abba2473709fd2c02a89ffd26db42276fc
Akinbande Gbenga <olugbengalium@gmail.com>
2023-03-05 16:32:07 +0100

    Initial Commit

---
+++ Recipee/RecipeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeeApp.swift
+//  Recipee
+//
+//  Created by Gbenga Abayomi on 05/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct RecipeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
